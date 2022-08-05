4273b4c78a6d4509c4c916c57faf9f8e638d5d84
edvan.ximenes <endvan@gmail.com>
2022-08-04 23:10:53 -0300

    try e catch HIERAQUIA

---
+++ src/main/java/excessoes/Fluxo.java
@@ -0,0 +1,22 @@
+package excessoes;
+
+public class Fluxo {
+
+    public static void main(String[] args) {
+        System.out.println("Início do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Início do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+    
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        throw new ArithmeticException("Deu erro");
+    }
+}
