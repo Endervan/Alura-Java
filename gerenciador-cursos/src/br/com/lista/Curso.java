730432b2b5424ef5360ceb17f26e3e0da98d8ee9
edvan.ximenes <endvan@gmail.com>
2023-02-21 14:31:28 -0300

    Coleções threadsafe

--- gerenciador-cursos/src/br/com/lista/Curso.java
+++ gerenciador-cursos/src/br/com/lista/Curso.java
@@ -49,6 +49,12 @@ public class Curso {
     public Set<Aluno> getAlunos() {
         return Collections.synchronizedSet(alunos);
 //        synchronizedSet ..monta lista em ordem alfabetica
+
+//        Uma das características mais interessantes de JVM é que ela sabe trabalhar em paralelo. Internamente isso é feito por meio de Threads que funcionam como pequenos processos dentro da JVM.
+//
+//        O problema é que as coleções que estamos usando até agora não foram feitas para serem manipuladas em paralelo. No entanto, nada impede que usemos um método da classe Collections para transformar uma coleção comum em uma coleção para threads. É justamente isso que o método faz, retorna um nova coleção que pode ser compartilhada entre threads sem perigos.
+//
+//                O tópico de Threads é importante pois elas são muito utilizadas dentro das bibliotecas que rodam no mundo Java. Na Alura, temos dois treinamentos dedicados a threads:
     }
 
 
