0b1f44bfe833a184d62089a6131d5237848dc0f2
edvan.ximenes <endvan@gmail.com>
2023-03-10 00:19:29 -0300

    Mais Streams, Collectors e APIs

--- java8/Java8/src/ExemploCursos.java
+++ java8/Java8/src/ExemploCursos.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExemploCursos {
 
@@ -20,17 +21,39 @@ public class ExemploCursos {
 
         // stream devolve algo com fluxo de objeto (corrente)
 //         predicate => criterio a ser adotado
-        cursos.stream().filter(c -> c.getAlunos() >= 100)
-                .map(Curso::getAlunos)
-                .forEach(System.out::println);
+//        cursos.stream().filter(c -> c.getAlunos() >= 100)
+//                .map(Curso::getAlunos)
+//                .forEach(System.out::println);
 
 
-        // somando valores cursos mairores dou == a 100
-        int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
-        System.out.println(sum);
+//        // somando valores cursos mairores dou == a 100
+//        OptionalDouble sum = cursos.stream()
+//                .filter(c -> c.getAlunos() >= 100)
+//                .mapToInt(Curso::getAlunos)
+//                .average();
+//
+//        System.out.println(sum);
+//
+//        // Optional ja tratar null automaticamente
+//        cursos.stream()
+//                .filter(c -> c.getAlunos() >= 100) // devolve stream
+//                .findAny()// devolve optional
+//                .ifPresent(c -> System.out.println(c.getNome()));
+
+//       Curso curso =  optionalCurso.orElse(null);
+//
+//        System.out.println(curso.getNome());
 
 
 //        cursos.forEach(curso -> System.out.println(curso.getNome()));
+
+        // toMap e forEach colectors recebe bi consumeres caso seria chave e o valor
+        cursos.parallelStream()// parallelStream trabalha paralelamente isso torna aplica sup rapida em ambiente que tenha muitos dados
+                .filter(c -> c.getAlunos() >= 100)
+                .collect(Collectors.toMap(
+                        Curso::getNome,
+                        Curso::getAlunos))
+                .forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
 
     }
 
