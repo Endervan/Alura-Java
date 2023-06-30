8532490025e10368a4345d36f0c3ab28f63fd2d0
edvan.ximenes <endvan@gmail.com>
2023-06-30 18:04:12 -0300

    Recebendo dados no Controller

--- scrimMatter/src/main/java/br/com/aluraA/scrimMatter/controller/FilmeController.java
+++ scrimMatter/src/main/java/br/com/aluraA/scrimMatter/controller/FilmeController.java
@@ -1,7 +1,9 @@
 package br.com.aluraA.scrimMatter.controller;
 
+import br.com.aluraA.scrimMatter.model.filme.DadosCadastraFilme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +12,12 @@ public class FilmeController {
 
     @GetMapping
     public String carregaPaginaFormulario() {
+        return "filmes/formulario";
+    }
+
+    @PostMapping
+    public String cadastraFilme(DadosCadastraFilme dados) {
+        System.out.println(dados);
         return "filmes/formulario";
     }
 
---
+++ scrimMatter/src/main/java/br/com/aluraA/scrimMatter/model/filme/DadosCadastraFilme.java
@@ -0,0 +1,4 @@
+package br.com.aluraA.scrimMatter.model.filme;
+
+public record DadosCadastraFilme(String nome, Integer duracao, Integer ano, String genero) {
+}
