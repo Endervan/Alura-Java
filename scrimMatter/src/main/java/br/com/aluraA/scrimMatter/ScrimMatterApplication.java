ace62736acd31e0470dd923fdf7742d40ed60b6c
edvan.ximenes <endvan@gmail.com>
2023-06-30 17:02:56 -0300

    Iniciando o projeto com Spring Initializr

---
+++ scrimMatter/src/main/java/br/com/aluraA/scrimMatter/ScrimMatterApplication.java
@@ -0,0 +1,13 @@
+package br.com.aluraA.scrimMatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrimMatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrimMatterApplication.class, args);
+	}
+
+}
---
+++ scrimMatter/src/test/java/br/com/aluraA/scrimMatter/ScrimMatterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aluraA.scrimMatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrimMatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
