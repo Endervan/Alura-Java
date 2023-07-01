d20b5348c865dd3476e75c907bbb6165522f8b21
edvan.ximenes <endvan@gmail.com>
2023-07-01 18:59:10 -0300

    Cadastrando filmes em memoria (List)

--- scrimMatter/src/main/java/br/com/aluraA/scrimMatter/controller/FilmeController.java
+++ scrimMatter/src/main/java/br/com/aluraA/scrimMatter/controller/FilmeController.java
@@ -1,14 +1,20 @@
 package br.com.aluraA.scrimMatter.controller;
 
 import br.com.aluraA.scrimMatter.model.filme.DadosCadastraFilme;
+import br.com.aluraA.scrimMatter.model.filme.Filme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
+
+    private final List<Filme> filmes = new ArrayList<>();
 
     @GetMapping
     public String carregaPaginaFormulario() {
@@ -17,9 +23,18 @@ public class FilmeController {
 
     @PostMapping
     public String cadastraFilme(DadosCadastraFilme dados) {
-        System.out.println(dados);
+        var filme = new Filme(dados);
+        filmes.add(filme);
+
+        System.out.println(filmes);
         return "filmes/formulario";
     }
 
 
 }
+
+//    Lançado oficialmente no Java 16, mas disponível desde o Java 14 de maneira experimental, o Record é um recurso que permite representar uma classe imutável, contendo apenas atributos, construtor e métodos de leitura, de uma maneira muito simples e enxuta.
+//
+//        Esse APAGAR código se encaixa perfeitamente quando precisamos representar os dados enviados em um formulário, já que o foco é apenas nos dados em si que serão recebidos, sem nenhum tipo de comportamento adicional.
+//
+//        Para se criar uma classe de dados imutável, sem a utilização do Record, era necessário escrever muito código. Vejamos um exemplo de uma classe que representa um telefone:
---
+++ scrimMatter/src/main/java/br/com/aluraA/scrimMatter/model/filme/Filme.java
@@ -0,0 +1,41 @@
+package br.com.aluraA.scrimMatter.model.filme;
+
+public class Filme {
+ private final String nome;
+ private final Integer duracaoEmMinutos;
+ private final Integer anoLancamento;
+ private final String genero;
+
+    public Filme(DadosCadastraFilme dados) {
+        this.nome = dados.nome();
+        this.duracaoEmMinutos = dados.duracao();
+        this.anoLancamento = dados.ano();
+        this.genero = dados.genero();
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "nome='" + nome + '\'' +
+                ", duracaoEmMinutos=" + duracaoEmMinutos +
+                ", anoLancamento=" + anoLancamento +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public Integer getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+}
