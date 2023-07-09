16b677844c35edf9b6e760b63b4960754859e9ab
edvan.ximenes <endvan@gmail.com>
2023-07-09 10:26:20 -0300

    Adicionando dependências

---
+++ api_spring_boot/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,5 @@
+package med.voll.api.endereco;
+
+public record DadosEndereco(String logradouro,String bairro,String cep,String crm,
+                            String cidade,String uf,String complemento,String numero) {
+}
---
+++ api_spring_boot/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(
+        String nome,
+        String email,
+        Especialidade especialidade,
+        DadosEndereco endereco
+) {
+}
