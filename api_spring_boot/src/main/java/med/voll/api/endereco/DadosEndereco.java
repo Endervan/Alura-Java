4546dfc3242f0df2b7f2bc4e768efe88d238baa0
edvan.ximenes <endvan@gmail.com>
2023-07-09 15:43:49 -0300

    Validação com Bean Validation

--- api_spring_boot/src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ api_spring_boot/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,5 +1,20 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String logradouro,String bairro,String cep,String crm,
-                            String cidade,String uf,String complemento,String numero) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+        @NotBlank
+        String logradouro,
+        @NotBlank
+        String bairro,
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep, String crm,
+        @NotBlank
+        String cidade,
+        @NotBlank
+        String uf,
+        String complemento,
+        String numero) {
 }
--- api_spring_boot/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api_spring_boot/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,12 +1,24 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
+
+        @NotBlank // verifica se nao ta vazio e null
         String nome,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String crm,
+        @NotNull
         Especialidade especialidade,
-        DadosEndereco endereco
+        @NotNull @Valid DadosEndereco endereco
 ) {
 }
