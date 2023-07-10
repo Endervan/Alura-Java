f6aa4e3e48d33ea4e55511ca2d8c325668740573
edvan.ximenes <endvan@gmail.com>
2023-07-09 21:40:05 -0300

    Atualizando dados

--- api_spring_boot/src/main/java/med/voll/api/controller/medicoController.java
+++ api_spring_boot/src/main/java/med/voll/api/controller/medicoController.java
@@ -1,13 +1,11 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,8 +24,18 @@ public class medicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao) {
-       return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        var medico = repository.getReferenceById(dados.id());  // trazendo objeto BD
+
+        // JPA detectar que ouve uma mundança no atribruto e faz update automatico
+        medico.atualizarInformacoes(dados);
 
     }
 
--- api_spring_boot/src/main/java/med/voll/api/medico/Medico.java
+++ api_spring_boot/src/main/java/med/voll/api/medico/Medico.java
@@ -41,6 +41,13 @@ public class Medico {
         this.endereco = new Endereco(dados.endereco());
         this.nome = dados.nome();
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) this.nome = dados.nome();
+        if (dados.telefone() != null) this.telefone = dados.telefone();
+        if (dados.endereco() != null) this.endereco.atualizarInformacoes(dados.endereco());
+
+    }
 }
 
 //DTO->Data Transfer Object
