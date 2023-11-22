2438869b5cbf2ab2af682712f871025165d8157e
edvan.ximenes <endvan@gmail.com>
2023-11-22 12:05:23 -0300

    heranca

---
+++ Java_udemy/Treinamento1/src/Heranca/Conta/BusinessAccount.java
@@ -0,0 +1,28 @@
+package Heranca.Conta;
+
+public class BusinessAccount extends Account {
+
+    private Double loadLimit;
+
+    public BusinessAccount() {
+        super();
+    }
+
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loadLimit) {
+        super(number, holder, balance);
+        this.loadLimit = loadLimit;
+    }
+
+    public Double getLoadLimit() {
+        return loadLimit;
+    }
+
+    public void setLoadLimit(Double loadLimit) {
+        this.loadLimit = loadLimit;
+    }
+
+    public void loan(double amount) {
+        balance += amount - 10;
+    }
+}
---
+++ Java_udemy/Treinamento1/src/Heranca/program.java
@@ -0,0 +1,7 @@
+package Heranca;
+
+public class program {
+    public static void main(String[] args) {
+
+    }
+}
