85ae3f5ec852e418a3c4f78b74c01be837b6e107
edvan.ximenes <endvan@gmail.com>
2024-01-22 11:28:43 -0300

    recuperando dados msql jdbc conector e excessoes exclusivas

--- Java_udemy/Treinamento1/src/db/DB.java
+++ Java_udemy/Treinamento1/src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -49,5 +47,28 @@ public class DB {
 
         }
     }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+
+        }
+    }
+
 
 }
