cb3861c3bdb58ad428b42d306dd27b4ae8751dce
edvan.ximenes <endvan@gmail.com>
2024-01-29 10:38:50 -0300

    reutilizando a instanciacao

--- Java_udemy/Treinamento1/src/db/apllication/ProgramTestBuscandoPorId.java
+++ Java_udemy/Treinamento1/src/db/apllication/ProgramTestBuscandoPorId.java
@@ -8,6 +8,8 @@ public class ProgramTestBuscandoPorId {
 
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=========test 1 : seller findById =========");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
