d513d0a3314177298b750008d1b5028bcc872a09
edvan.ximenes <endvan@gmail.com>
2024-01-29 15:09:57 -0300

    delete

--- Java_udemy/Treinamento1/src/db/model/dao/impl/SellerDaoJDBC.java
+++ Java_udemy/Treinamento1/src/db/model/dao/impl/SellerDaoJDBC.java
@@ -87,7 +87,18 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement("delete from  seller where Id=?");
+            st.setInt(1,id);
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DBException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
