8faa5a4812ae2c5dc015179b64944f65fc87f178
Ender 2024 <endvan@gmail.com>
2024-03-14 22:31:09 -0300

    configurando e criando 1 tabela usando h2 em memoria

--- Java_udemy/course_spring/src/main/java/com/educandowebender/course_spring/entities/User.java
+++ Java_udemy/course_spring/src/main/java/com/educandowebender/course_spring/entities/User.java
@@ -1,11 +1,17 @@
 package com.educandowebender.course_spring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id // usa annotacion pra mostra jpa qual campo sera chve primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
