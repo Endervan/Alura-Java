93614169fce8dfff2f95a1505036c33274723213
edvan.ximenes <endvan@gmail.com>
2024-03-18 21:00:46 -0300

    associacao muitos pra um e um pra muitos

--- Java_udemy/course_spring/src/main/java/com/educandowebender/course_spring/entities/User.java
+++ Java_udemy/course_spring/src/main/java/com/educandowebender/course_spring/entities/User.java
@@ -3,6 +3,8 @@ package com.educandowebender.course_spring.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client") // um pra muitos
+    private final List<Order> orders = new ArrayList<>();
+
 
     public User() {
     }
@@ -68,6 +74,11 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
 
     @Override
     public boolean equals(Object o) {
