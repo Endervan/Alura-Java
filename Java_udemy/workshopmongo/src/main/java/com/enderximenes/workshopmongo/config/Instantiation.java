aadcae09cc9af4c4cd730f47ef682000cda5d6e2
Ender 2024 <endvan@gmail.com>
2024-05-24 21:02:20 -0300

    Mongo : create post com user aninhado

--- Java_udemy/workshopmongo/src/main/java/com/enderximenes/workshopmongo/config/Instantiation.java
+++ Java_udemy/workshopmongo/src/main/java/com/enderximenes/workshopmongo/config/Instantiation.java
@@ -1,27 +1,42 @@
 package com.enderximenes.workshopmongo.config;
 
+import com.enderximenes.workshopmongo.domain.Post;
 import com.enderximenes.workshopmongo.domain.User;
+import com.enderximenes.workshopmongo.reposity.PostRepository;
 import com.enderximenes.workshopmongo.reposity.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         // limpando dados no Mongo
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "marian", "mariaMaio@email.com");
         User joao = new User(null, "joao", "joao@email.com");
         User charles = new User(null, "charles ", "charles@email.com");
 
-        userRepository.saveAll(Arrays.asList(maria,joao,charles));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viajem", "vou viajar pra sao paulo", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom", "Acordei feliz hoje", maria);
+
+        userRepository.saveAll(Arrays.asList(maria, joao, charles));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ Java_udemy/workshopmongo/src/main/java/com/enderximenes/workshopmongo/domain/Post.java
@@ -0,0 +1,78 @@
+package com.enderximenes.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Post implements Serializable {
+    private static final long seriaversionlUID = 1L;
+
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+
+    public Post() {
+    }
+
+    public Post(String id, Date date, String title, String body, User author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Post post)) return false;
+        return getId().equals(post.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
