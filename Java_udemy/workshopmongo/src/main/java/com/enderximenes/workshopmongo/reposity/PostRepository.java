ee79802be1a7494e887852ce1babf2ace3d0dbdf
Ender 2024 <endvan@gmail.com>
2024-05-24 23:26:44 -0300

    Mongo : consulta varias @Queries

--- Java_udemy/workshopmongo/src/main/java/com/enderximenes/workshopmongo/reposity/PostRepository.java
+++ Java_udemy/workshopmongo/src/main/java/com/enderximenes/workshopmongo/reposity/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -16,5 +17,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     // gerando consulta automaticas com spring
     List<Post> findByTitleIsContainingIgnoreCase(String text);
+
+    @Query("{$and: [{date:{$gte:?1 }},{date:{$lte:?2 }},{$or: [{'title': {$regex: ?0,$options: 'i'}},{'body': {$regex: ?0,$options: 'i'}},{'comments.texto': {$regex: ?0,$options: 'i'}}]}]}")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- Java_udemy/workshopmongo/src/main/java/com/enderximenes/workshopmongo/resource/util/URL.java
+++ Java_udemy/workshopmongo/src/main/java/com/enderximenes/workshopmongo/resource/util/URL.java
@@ -2,11 +2,24 @@ package com.enderximenes.workshopmongo.resource.util;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
     public static String decodeParam(String text) {
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
 
+    public static Date convertDate(String textDate, Date defaultDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultDate;
+        }
     }
 }
