3522a15d22c3d27ace1caabc12d103c0e63be45a
Ender <endvan@gmail.com>
2025-05-28 20:59:01 -0300

    sobrecarga de metodos comer estaticos

---
+++ exerciciosUdemy2025/src/main/java/oo/heranca/polimorfismo/Pessoa.java
@@ -0,0 +1,33 @@
+package oo.heranca.polimorfismo;
+
+public class Pessoa {
+
+    private double peso;
+
+    public Pessoa(double peso) {
+        setPeso(peso);
+    }
+
+    // sobrecarga de metodos comer estaticos
+    public void comer(Arroz arroz) {
+        this.peso += arroz.getPeso();
+    }
+
+    public void comer(Feijao feijao) {
+        this.peso += feijao.getPeso();
+    }
+
+    public void comer(Sorvete sorvete) {
+        this.peso += sorvete.getPeso();
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        if (peso >= 0) {
+            this.peso = peso;
+        }
+    }
+}
