79fd58fb6050e09c2943977bdde655587eec969a
Ender <endvan@gmail.com>
2025-05-28 21:57:54 -0300

    metodos interfaces abstract

--- exerciciosUdemy2025/src/main/java/oo/heranca/desafio/Esportivo.java
+++ exerciciosUdemy2025/src/main/java/oo/heranca/desafio/Esportivo.java
@@ -2,6 +2,8 @@ package oo.heranca.desafio;
 
 public interface Esportivo {
 
+    // metodos abstract nao tei corpo
+    // todos metodo esteja numa interface e explicidameente abstract e public
     void ligarTurbo();
     void desligarTurbo();
 }
