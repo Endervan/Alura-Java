453712a1457671837d6cc61e77b987993842152c
ealves_noot <endvan@gmail.com>
2025-06-09 21:07:04 -0300

    supplier

---
+++ exerciciosUdemy2025/src/main/java/lambada/SupplierComposicao.java
@@ -0,0 +1,14 @@
+package lambada;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierComposicao {
+    public static void main(String[] args) {
+
+        Supplier<List<String>> supplier = () -> Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+
+        System.out.println(supplier.get());
+    }
+}
