12c3eaa41038fa6bd2a295c4e48744b094eefd8e
ealves_noot <endvan@gmail.com>
2025-06-09 21:07:52 -0300

    supplier 1

--- exerciciosUdemy2025/src/main/java/lambada/SupplierComposicao.java
+++ exerciciosUdemy2025/src/main/java/lambada/SupplierComposicao.java
@@ -7,6 +7,7 @@ import java.util.function.Supplier;
 public class SupplierComposicao {
     public static void main(String[] args) {
 
+        // tipo funcional que nao receber nenhum mas e pode fornecer algo
         Supplier<List<String>> supplier = () -> Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
 
         System.out.println(supplier.get());
