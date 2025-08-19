25247fbe5c7b8c08fc82cb9ac02799f93d43baa5
ealves_noot <endvan@gmail.com>
2025-08-18 21:32:39 -0300

    update Cliente class to use cascade persist for Assento relationship; add new example for Cliente and Assento creation uso cascade persit

--- jpa/exercicio-jpa/exercicio-jpa/src/main/java/infra/DAO.java
+++ jpa/exercicio-jpa/exercicio-jpa/src/main/java/infra/DAO.java
@@ -1,8 +1,11 @@
 package infra;
 
+import modelo.umpraum.Cliente;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class DAO<E> {
@@ -14,19 +17,19 @@ public class DAO<E> {
     static {
         try {
             emf = Persistence
-                    .createEntityManagerFactory("exercicio-jpa");
+                    .createEntityManagerFactory("exercicios-jpa");
         } catch (Exception e) {
-            System.err.println("Erro ao inicializar o EntityManagerFactory: " + e.getMessage());
+            // logar -> log4j
         }
+    }
+
+    public DAO() {
+        this(null);
     }
 
     public DAO(Class<E> classe) {
         this.classe = classe;
-        this.em = emf.createEntityManager(); // Inicialização do EntityManager
-    }
-
-    public E obterPorId(Object id) {
-        return em.find(classe, id);
+        em = emf.createEntityManager();
     }
 
     public DAO<E> abrirT() {
@@ -48,22 +51,39 @@ public class DAO<E> {
         return this.abrirT().incluir(entidade).fecharT();
     }
 
-    // usando sobrecarga de obterTodos para permitir a obtenção de todos os registros
-    // com ou sem limites e deslocamento
+    public E obterPorID(Object id) {
+        return em.find(classe, id);
+    }
+
     public List<E> obterTodos() {
         return this.obterTodos(10, 0);
     }
 
-    public List<E> obterTodos(int limit, int offset) {
+    public List<E> obterTodos(int qtde, int deslocamento) {
         if (classe == null) {
-            throw new UnsupportedOperationException("Classe não definida para o DAO.");
+            throw new UnsupportedOperationException("Classe nula.");
         }
 
-        String jpql = "SELECT e FROM " + classe.getName() + " e";
-        return em.createQuery(jpql, classe)
-                .setMaxResults(limit)
-                .setFirstResult(offset)
-                .getResultList();
+        String jpql = "select e from " + classe.getName() + " e";
+        TypedQuery<E> query = em.createQuery(jpql, classe);
+        query.setMaxResults(qtde);
+        query.setFirstResult(deslocamento);
+        return query.getResultList();
+    }
+
+    public List<E> consultar(String nomeConsulta, Object... params) {
+        TypedQuery<E> query = em.createNamedQuery(nomeConsulta, classe);
+
+        for (int i = 0; i < params.length; i += 2) {
+            query.setParameter(params[i].toString(), params[i + 1]);
+        }
+
+        return query.getResultList();
+    }
+
+    public E consultarUm(String nomeConsulta, Object... params) {
+        List<E> lista = consultar(nomeConsulta, params);
+        return lista.isEmpty() ? null : lista.get(0);
     }
 
     public void fechar() {
--- jpa/exercicio-jpa/exercicio-jpa/src/main/java/teste/umpraum/ObterClienteAssento.java
+++ jpa/exercicio-jpa/exercicio-jpa/src/main/java/teste/umpraum/ObterClienteAssento.java
@@ -9,12 +9,12 @@ public class ObterClienteAssento {
     public static void main(String[] args) {
         DAO<Cliente> daoCliente = new DAO<>(Cliente.class);
 
-        Cliente cliente = daoCliente.obterPorId(1L);
+        Cliente cliente = daoCliente.obterPorID(1L);
         System.out.println(cliente.getAssento().getNome()); // assento
         System.out.println(cliente.getAssento().getCliente().getNome()); // cliente daquele assento
 
         DAO<Assento> daoAssento = new DAO<>(Assento.class);
-        System.out.println(daoAssento.obterPorId(1L).getCliente().getNome()); // assento daquele cliente
+        System.out.println(daoAssento.obterPorID(1L).getCliente().getNome()); // assento daquele cliente
 
 
         daoCliente.fechar();
