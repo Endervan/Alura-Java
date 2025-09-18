d5391f084b11ff381748e64b1fa268692f9be81c
ealves_noot <endvan@gmail.com>
2025-09-17 22:06:03 -0300

    Miniatura da aula 426. Passando Parâmetros para Web Service

--- JavaFx/exercicios_spring_boot/src/main/java/org/example/ender/controls/ClienteController.java
+++ JavaFx/exercicios_spring_boot/src/main/java/org/example/ender/controls/ClienteController.java
@@ -2,6 +2,7 @@ package org.example.ender.controls;
 
 import org.example.ender.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class ClienteController {
     @GetMapping(path = "/qualquer")
     public Cliente obterCliente() {
         return new Cliente(1, "Ana", "123.456.789-00");
+    }
+
+    @GetMapping(path = "/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id) {
+        return new Cliente(id, "maria", "999.456.999-00");
     }
 }
